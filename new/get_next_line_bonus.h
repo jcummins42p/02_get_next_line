ddcb96389c513daa29f09c540a335f1384006d3d
John Cummins <jcummins@c2r5s2.42prague.com>
2023-12-06 19:12:15 +0100

    Updated heade bonus name

--- new/get_next_line_bonus.h
+++ new/get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jcummins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 16:41:06 by jcummins          #+#    #+#             */
-/*   Updated: 2023/12/04 16:56:48 by jcummins         ###   ########.fr       */
+/*   Updated: 2023/12/06 19:11:44 by jcummins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <unistd.h>
 # include <stdio.h>
