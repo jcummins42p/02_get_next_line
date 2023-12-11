9a54b6fc66a4eab78a9f83083ab2f1d4a7a7d0af
John Cummins <jcummins@c2r5s2.42prague.com>
2023-12-11 13:38:49 +0100

    updated .h files

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jcummins <jcummins@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 10:26:32 by jcummins          #+#    #+#             */
-/*   Updated: 2023/12/11 13:16:38 by jcummins         ###   ########.fr       */
+/*   Updated: 2023/12/11 13:35:43 by jcummins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,22 @@
 # include <fcntl.h>
 # include <stdlib.h>
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 64
+# endif
+
+/* GNL */
+char	*split_newline(char *str);
+char	*get_buffer(int fd);
+int		extend_line(int fd, char **line);
+char	*get_line(int fd);
 char	*get_next_line(int fd);
+
+/* UTILS */
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlen(const char *s);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 char	*ft_strdup(const char *s);
 int		is_complete(char *str);
-
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 50
-# endif
 
 #endif
